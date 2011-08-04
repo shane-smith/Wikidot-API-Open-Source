0c1b970e9c0430511b94d60b8994aadfdc7a56e4
Shane Smith <shane@Shane-Smiths-MacBook.local>
2011-08-04 18:41:04 +0930

    Added the first class: WikidotAPI.java
    The class doesn't actually do anything at the moment. I'll implement it
    soon.

---
+++ WikidotAPI.java
@@ -0,0 +1,15 @@
+/**
+ * Wikidot API Open Source Project
+ * Lead developer: Shane Smith (leiger)
+ * 
+ * This document last modified: 2011-08-04
+ **/
+
+public class WikidotAPI
+{
+	// Constructor
+	public WikidotAPI()
+	{
+		// TODO
+	}
+}
