888dfb915144edff42dc5f2ba97709160cabe6dd
Shane Smith <shane@192.168.2.2>
2012-08-21 22:55:00 +0930

    Added license.txt, started working on WikidotAPI.java

--- WikidotAPI.java
+++ WikidotAPI.java
@@ -1,15 +1,50 @@
 /**
  * Wikidot API Open Source Project
+ * Java API library
+ * 
  * Lead developer: Shane Smith (leiger)
  * 
- * This document last modified: 2011-08-04
+ * Licensed under GNU GPL v3
  **/
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
 public class WikidotAPI
 {
-	// Constructor
-	public WikidotAPI()
+	private static String user;
+	private static String key;
+	private static final String SERVER_URL = "https://www.wikidot.com/xml-rpc-api.php";
+	private static XmlRpcClientConfigImpl config;
+	private static XmlRpcClient client;
+	
+	// Get the client
+	public XmlRpcClient getClient()
 	{
-		// TODO
+		return client;
+	}
+	
+	// Constructor
+	public WikidotAPI(String applicationName, String apiKey) throws MalformedURLException
+	{
+		// Store variables
+		this.user = applicationName;
+		this.key = apiKey;
+		
+		// Set client configuration options
+		config = new XmlRpcClientConfigImpl();
+		config.setServerURL(new URL(SERVER_URL));
+		config.setBasicUserName(user);
+		config.setBasicPassword(key);
+		config.setEnabledForExtensions(true);
+		config.setConnectionTimeout(10 * 1000);
+		config.setReplyTimeout(30 * 1000);
+		
+		// Initialise the client object
+		client = new XmlRpcClient();
+		client.setConfig(config);
 	}
 }
