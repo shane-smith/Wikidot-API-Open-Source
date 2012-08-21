9a7c5f51879b2b39fc4233657c12535a72b92b90
Shane Smith <www.shane-smith.com/contact>
2012-08-22 00:15:25 +0930

    @Override getParser(...)

--- com/shanesmith/wikidot/WikidotTypeFactory.java
+++ com/shanesmith/wikidot/WikidotTypeFactory.java
@@ -11,8 +11,13 @@
 
 package com.shanesmith.wikidot;
 
+import org.apache.ws.commons.util.NamespaceContextImpl;
 import org.apache.xmlrpc.common.TypeFactoryImpl;
 import org.apache.xmlrpc.common.XmlRpcController;
+import org.apache.xmlrpc.common.XmlRpcStreamConfig;
+import org.apache.xmlrpc.parser.NullParser;
+import org.apache.xmlrpc.parser.TypeParser;
+import org.apache.xmlrpc.serializer.NullSerializer;
 
 /**
  * This class extends TypeFactoryImpl and caters for the Wikidot API's use of "<NIL>" as a data type.
@@ -20,10 +25,28 @@ import org.apache.xmlrpc.common.XmlRpcController;
  */
 public class WikidotTypeFactory extends TypeFactoryImpl
 {
+	/**
+	 * TODO: Documentation
+	 * @param pController
+	 */
 	public WikidotTypeFactory(XmlRpcController pController)
 	{
 		super(pController);
 	}
 	
-	// TODO
+	/**
+	 * TODO: Documentation
+	 */
+	@Override
+	public TypeParser getParser(XmlRpcStreamConfig pConfig, NamespaceContextImpl pContext, String pURI, String pLocalName)
+	{
+		if ("".equals(pURI) && NullSerializer.NIL_TAG.equals(pLocalName))
+		{
+			return new NullParser();
+		}
+		else
+		{
+			return super.getParser(pConfig, pContext, pURI, pLocalName);
+		}
+	}
 }
